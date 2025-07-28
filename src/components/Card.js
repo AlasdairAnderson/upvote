f34f7409fa84da6c01c270df1e669c0dfa9cff2b
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2025-07-28 10:20:14 +0100

    update to cards CSS to improve layout and post stats

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 
 
 export const Card = ({card}) => {
-  const { upvotes, downvotes, num_comments, totalvotes, title, post_content } = card
+  const { upvotes, downvotes, num_comments, title, post_content } = card
 
   const contentType = (content) => {
     if (!content || !content.type) {
@@ -19,42 +19,38 @@ export const Card = ({card}) => {
         break;
       case 'text':
         return(<p className="content">{content.content}</p>);
+        break;
       default:
         return(<p className="content">No content found</p>);
     }
   }
 
-  const StackedProgressBar = ({ segments }) => {
-    return(<div className="progress-bar">
-      {segments.map((segment, index) => {
-        return(
-        <div
-          key={`${segment.percentage}${index}`} className="progress-segment"
-          style={{
-            width: `${segment.percentage}%`,
-            backgroundColor: segment.color
-          }}
-          title={`${segment.lable}: ${segment.percentage}`}></div>);
-      })}
-    </div>);
+  const roundStats = (stat) => {
+    if (stat < 1000) {
+      return stat;
+    } else {
+      const roundedStat = Math.round(stat/1000);
+      return `${roundedStat}K`;
+    }
   }
 
   return(
     <article className="contentCard">
       <section className="card-content">
         { contentType(post_content) }
-      </section>
-      <section className="card-information">
-        <h2>{title}</h2>
-        <div className="card-stats">
-          <div className="stats">
-            <img src="/UpvoteIcon.svg" alt="upvote icon"/>
-            <StackedProgressBar segments={[{label:"upvote",percentage: Math.round(upvotes/totalvotes * 100), color: "#FF4500"},{lable:"downvote", percentage: Math.round(downvotes/totalvotes * 100), color: "#7193FE"}]} />
-            <img src="/DownvoteIconv2.png" alt="downvote icon"/>
-          </div>
-          <div className="stats">
-            <img src="/MessageIcon.svg" alt="message icon"/>
-            <StackedProgressBar segments={[{label:"upvote",percentage: Math.round(num_comments/1500 * 100), color: "#fff"}]} />
+        <div className="card-information">
+          <h2>{title}</h2>
+          <div className="card-information-footer">
+            <div className="card-information-footer-info">
+                <img src="/UpvoteIcon.svg" alt="Upvotes"/>
+                <p>{roundStats(upvotes)}</p>
+                <img src="/DownvoteIcon.svg" alt="Downvotes"/>
+                <p>{roundStats(downvotes)}</p>
+            </div>
+            <div className="card-information-footer-info">
+                <img src="/MessageIcon.svg" alt="Comments"/>
+              <p>{roundStats(num_comments)}</p>
+            </div>
           </div>
         </div>
       </section>
