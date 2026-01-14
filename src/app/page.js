ed57d138c009ecf8a5bec89c315ae9d9c5d1d2be
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2026-01-14 18:42:00 +0000

    Start of drawing out SVG path for the new card swipe functionality

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,9 @@ export default function Home() {
       <div className='voting-sections'>
         <section className='downvote section'></section>
         <section className='novote section'>
+          <svg viewBox='0 0 100 200' width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" id='vote-path'>
+            <path d="M0,10 Q80,10 100,30 Q120,10 180,10 L200,10" stroke="black" fill="transparent"/>
+          </svg>
           <CardStack/>
         </section>
         <section className='upvote section'></section>
