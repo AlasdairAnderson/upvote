bb45bcb1ed40a02b02edc10f1d23de2fce873338
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2026-01-15 06:14:23 +0000

    update to SVG to get prefered shape

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
       <div className='voting-sections'>
         <section className='downvote section'></section>
         <section className='novote section'>
-          <svg viewBox='0 0 100 200' width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" id='vote-path'>
+          <svg viewBox='0 0 200 30' width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" id='vote-path'>
             <path d="M0,10 Q80,10 100,30 Q120,10 180,10 L200,10" stroke="black" fill="transparent"/>
           </svg>
           <CardStack/>
