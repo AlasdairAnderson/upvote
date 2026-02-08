a63c7cca6c58261409e2f8676f43426bfb1a7dc0
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2026-02-08 19:27:55 +0000

    Implementation of mapping cardOpacity and cardRotation to the current distance of the card along the offsetpath as well as the dispatching of the upvote and downvote actions when the card moves past a predefined point

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
-import { elementCenterIsInZone} from "@/lib/utils/dropzones";
+import React, { useRef, useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { addUpvote, addDownvote, deleteVotedCard, selectActiveCard } from "@/lib/features/cards/cardsSlice";
 
@@ -7,9 +6,9 @@ export const Card = ({ animation, card, cardPoistioning, onPointerDragStart, onP
   const { upvotes, downvotes, num_comments, title, post_content, id} = card
   const dispatch = useAppDispatch();
   const activeCard = useAppSelector(selectActiveCard);
-  const elRef = useRef(null);
   const [ cardOffsetDistance, setCardOffsetDistance ] = useState('50%') ;
-  const [ cardOffsetRotation, SetCardOffsetRotation ] = useState('0deg');
+  const [ cardOffsetRotation, setCardOffsetRotation ] = useState('0deg');
+  const [ cardOpacity, setCardOpacity ] = useState(1);
 
   const contentType = (content) => {
     if (!content || !content.type) {
@@ -42,55 +41,55 @@ export const Card = ({ animation, card, cardPoistioning, onPointerDragStart, onP
     }
   }
 
-  const currentDistance = (clientX) => {
+  const currentOffset = (clientX) => {
     if (cardPoistioning.mouseStartingPosition.x !== null){
       // update the offset distance of the card so that it moves along the offset-path alongside the users drag
-      console.log(cardPoistioning.mouseStartingPosition);
       const DeltaX = clientX - cardPoistioning.mouseStartingPosition.x;
-      console.log(DeltaX);
       // Calculate the percentage distance with 50+ (deltx/totalpathwidth * 100)
       const cardDistance = 50 + (DeltaX / 400 * 100);
-      console.log(`cardDistance ${cardDistance}%`);
       setCardOffsetDistance(`${cardDistance}%`);
-    } else {
-      setCardOffsetDistance('50%');
-    }
-  }
-
-  const currentRotation = (clientX) => {
-    if (cardPoistioning.mouseStartingPosition.x !== null){
-      //strip cardOffsetDistance string and convert it to a number
-      const currentCardDistance = Number(cardOffsetDistance.substring(0, cardOffsetDistance.length - 1));
-      console.log(currentCardDistance);
-      if(currentCardDistance >= 70){
-        console.log(`cardOffsetRotation: ${cardOffsetRotation}`)
-        SetCardOffsetRotation('10deg')
+      // Calculate the rotation
+      const cardRoation = (cardDistance / 2) - 25;
+      if(cardRoation < 20 && cardRoation > -20 ){
+        setCardOffsetRotation(`${cardRoation}deg`);
       }
-    } else {
-      SetCardOffsetRotation('0deg')
+      // Calcualte card opacity
+      let currentOpacity = (cardDistance / 50);
+      // Count down from 1 to 0 if past the mid point
+      if(cardDistance > 50){
+        currentOpacity = 1 - (currentOpacity - 1);
+      }
+      if(currentOpacity >= 0){
+        setCardOpacity(currentOpacity);
+      }
     }
   }
 
   const handleOnDragStop = (event) => {
     const card = document.getElementById(id);
+    const currentDistance = cardOffsetDistance.substring(0, cardOffsetDistance.length - 1);
+    console.log(cardOffsetDistance);
+    console.log(`currentDistance ${currentDistance}`);
     if(!card) return;
-
-    /*
-    if(upvoteIntersect){
+    if(currentDistance <= 20){
+      console.log('addUpvote');
       dispatch(addUpvote(activeCard));
       dispatch(deleteVotedCard(activeCard));
-    }
-    if(downvoteIntersect){
-      console.log(`downvoteIntersect: ${downvoteIntersect}`);
+    } else if(currentDistance >= 80){
+      console.log('addDownvote')
       dispatch(addDownvote(activeCard));
       dispatch(deleteVotedCard(activeCard));
-    }*/
+    }
+    // Reset Card
     onPointerDragStop(event);
+    setCardOffsetDistance('50%');
+    setCardOffsetRotation('0deg');
+    setCardOpacity(1);
   }
 
   return(
-    <li id={id} data-testid="card" style={{offsetDistance: cardOffsetDistance, offsetRotate: cardOffsetRotation}} onPointerDown={(event) => {onPointerDragStart(event, event.clientX, event.clientY)} } onPointerMove={(event) => {currentDistance(event.clientX); currentRotation(event.clientX)}} onPointerUp={(event) => {handleOnDragStop(event)}} className={`card-stack__item ${animation}`}>
-        <section className="card">
+    <li id={id} data-testid="card" style={{offsetDistance: cardOffsetDistance, offsetRotate: cardOffsetRotation}} onPointerDown={(event) => {onPointerDragStart(event, event.clientX, event.clientY)} } onPointerMove={(event) => {currentOffset(event.clientX);}} onPointerUp={(event) => {handleOnDragStop(event)}} onTouchStart={(event) => {onPointerDragStart(event, event.touches[0].clientX, event.touches[0].clientY)}} onTouchMove={(event) => {currentOffset(event.touches[0].clientX);}} onTouchEnd={(event) => {handleOnDragStop(event)}} className={`card-stack__item ${animation}`}>
+        <section className="card" style={{opacity: cardOpacity}}>
           { contentType(post_content) }
           <div className="card__information">
             <h2>{title}</h2>
